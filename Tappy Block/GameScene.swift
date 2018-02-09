7a1aa4e7872f23ea65940a77348a0f32784530e7
Alan Turing <turing@alan.local>
2018-02-09 16:05:32 +0900

    Trying to add scoring features and have the loop update inside of a nodes run

--- Tappy Block/GameScene.swift
+++ Tappy Block/GameScene.swift
@@ -9,6 +9,33 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    //Score
+    var score = 0
+    var hiScore = 0
+    
+    func updateScore(){
+        while true {
+            score+=2
+        }
+    }
+    
+    func makeHUD() {
+        let scoreLabel = SKLabelNode()
+        let hiScoreLabel = SKLabelNode()
+        
+        //scoreLabel.position = CGPoint(x: frame.minX-50, y: frame.minY-50)
+        scoreLabel.position = CGPoint(x: frame.minX+50, y: frame.maxY-50)
+        scoreLabel.color = UIColor.white
+        scoreLabel.fontName = "Courier"
+        scoreLabel.fontSize = 20
+        scoreLabel.text = "Score: \(score)"
+        addChild(scoreLabel)
+        //run(SKAction(updateScore()))
+        
+        
+        hiScoreLabel.position = CGPoint(x: frame.minX-200, y: frame.minY-50)
+        addChild(hiScoreLabel)
+    }
     
     let bird = SKSpriteNode(color: UIColor.red, size: CGSize(width: 20.0, height: 20.0))
     let birdFlap = SKAction.moveBy(x: 0.0, y: 50.0, duration: 0.5)
@@ -18,11 +45,16 @@ class GameScene: SKScene {
     let wallMove = SKAction.repeatForever(SKAction.moveBy(x: -20.0, y: 0.0, duration: 0.5))
 
     override func didMove(to view: SKView) {
+        
+        makeHUD()
+        
         bird.position = CGPoint(x: frame.minX+50, y: frame.midY)
        // bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.width/2)
         addChild(bird)
         
-        
+        if bird.intersects(wall) {
+            bird.color = UIColor.yellow
+        }
         
         
         wall.anchorPoint = CGPoint.zero
@@ -37,12 +69,11 @@ class GameScene: SKScene {
        // physicsBody = SKPhysicsBody(edgeChainFrom: CGPath(frame))
         
         bird.run(SKAction.repeatForever(birdFall))
-
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         bird.run(birdFlap)
-        
+      
     }
     
 }
